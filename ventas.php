a091f65ec051c59b606ec3abb740404a59063017
Juan Rojo <jjrojoa@gmail.com>
2022-11-03 10:53:51 -0500

    Arreglados links estilos

--- ventas.php
+++ ventas.php
@@ -6,84 +6,82 @@ $ventas = $sentencia->fetchAll(PDO::FETCH_OBJ);
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Ventas</title>
-	
-	<link rel="stylesheet" href="./css/fontawesome-all.min.css">
-	
-</head>
-<body>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#">POS</a>
-			</div>
-			<div id="navbar" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="./index.php">Productos</a></li>
-					<li><a href="./vender.php">Vender</a></li>
-					<li><a href="./ventas.php">Ventas</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	<div class="container">
-		<div class="row">
 
-	<div class="col-xs-12">
-		<h1>Ventas</h1>
-		<div>
-			<a class="btn btn-success" href="./vender.php">Nueva <i class="fa fa-plus"></i></a>
-		</div>
-		<br>
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th>Número</th>
-					<th>Fecha</th>
-					<th>Productos vendidos</th>
-					<th>Total</th>
-					<th>Ticket</th>
-					<th>Eliminar</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($ventas as $venta){ ?>
-				<tr>
-					<td><?php echo $venta->id ?></td>
-					<td><?php echo $venta->fecha ?></td>
-					<td>
-						<table class="table table-bordered">
-							<thead>
-								<tr>
-									<th>Código</th>
-									<th>Descripción</th>
-									<th>Cantidad</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach(explode("__", $venta->productos) as $productosConcatenados){ 
-								$producto = explode("..", $productosConcatenados)
-								?>
-								<tr>
-									<td><?php echo $producto[0] ?></td>
-									<td><?php echo $producto[1] ?></td>
-									<td><?php echo $producto[2] ?></td>
-								</tr>
-								<?php } ?>
-							</tbody>
-						</table>
-					</td>
-					<td><?php echo $venta->total ?></td>
-					<td><a class="btn btn-info" href="<?php echo "imprimirTicket.php?id=" . $venta->id?>"><i class="fa fa-print"></i></a></td>
-					<td><a class="btn btn-danger" href="<?php echo "eliminarVenta.php?id=" . $venta->id?>"><i class="fa fa-trash"></i></a></td>
-				</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-	</div>
-		</div>
-	</div>
+<head>
+  <meta charset="UTF-8">
+  <title>Ventas</title>
+
+  <link rel="stylesheet" href="./css/fontawesome-all.min.css">
+  <link rel="stylesheet" href="./css/styles.css">
+
+</head>
+
+<body>
+  <nav class="navbar-container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">POS</a>
+    </div>
+    <div class="navbar-links">
+      <ul>
+        <a class="nav-link" href="./index.php">Productos</a>
+        <a class="nav-link" href="./vender.php">Vender</a>
+        <a class="nav-link" href="./ventas.php">Ventas</a>
+      </ul>
+    </div>
+  </nav>
+  <div class="main">
+
+    <h1 class="title">Ventas</h1>
+    <div>
+      <a class="button btn-new" href="./vender.php">Nueva <i class="fa fa-plus"></i></a>
+    </div>
+    <br>
+    <table class="table">
+      <thead class="table-head">
+        <tr>
+          <th>Número</th>
+          <th>Fecha</th>
+          <th>Productos vendidos</th>
+          <th>Total</th>
+          <th>Ticket</th>
+          <th>Eliminar</th>
+        </tr>
+      </thead>
+      <tbody class="table-body">
+        <?php foreach ($ventas as $venta) { ?>
+          <tr>
+            <td><?php echo $venta->id ?></td>
+            <td><?php echo $venta->fecha ?></td>
+            <td>
+              <table class="table">
+                <thead class="table-head">
+                  <tr>
+                    <th>Código</th>
+                    <th>Descripción</th>
+                    <th>Cantidad</th>
+                  </tr>
+                </thead>
+                <tbody class="table-body">
+                  <?php foreach (explode("__", $venta->productos) as $productosConcatenados) {
+                    $producto = explode("..", $productosConcatenados)
+                  ?>
+                    <tr>
+                      <td><?php echo $producto[0] ?></td>
+                      <td><?php echo $producto[1] ?></td>
+                      <td><?php echo $producto[2] ?></td>
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
+            </td>
+            <td><?php echo $venta->total ?></td>
+            <td><a class="button btn-edit" href="<?php echo "imprimirTicket.php?id=" . $venta->id ?>"><i class="fa fa-print"></i></a></td>
+            <td><a class="button btn-delete" href="<?php echo "eliminarVenta.php?id=" . $venta->id ?>"><i class="fa fa-trash"></i></a></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </body>
+
 </html>
